d3bd6d36c4f3556b25450ff3702921dbdf98c238
ttn_js <ttn.js001@gmail.com>
2017-03-18 18:58:52 -0500

    drop down nav done

--- js/script.js
+++ js/script.js
@@ -15,20 +15,40 @@ $(document).ready(function(){
 		$('.tab-content').removeClass('current');
 
 		$(this).addClass('current');
-		$("#"+tab_id).addClass('current');
+		$('#'+tab_id).addClass('current');
 	});
 
 	//archive
 	var allPanels = $('.accordion > .inner').hide();
 
 	$('.accordion > a').click(function() {
-	  $(this).slideDown(500);
-	  $(this).toggleClass('selected');
-	  $(this).next().slideToggle(function(){});
-
-	  return false;
+		$(this).slideDown(500);
+	  	$(this).toggleClass('selected');
+	  	$(this).next().slideToggle(function(){
+	  	});
+	  	return false;
 	});
-	
+
+	function toggleNav(){
+		$('#drop-nav').slideToggle('slow');
+
+		// alert($('#logo').attr('src'));
+
+		if($('#logo').attr('src') == 'images/nav_logo.png'){
+			$(this).attr('src', 'images/nav_open_logo.png');
+		} else {
+			$(this).attr('src', 'images/nav_logo.png');
+		}
+
+	}
+
+	$('#logo').on('click', toggleNav);
+	// $(window).on('click', function(){
+	// 	if($('#drop-nav').css('display') == 'block'){
+	// 		toggleNav();
+	// 	}
+	// });
+
 })
 
 
